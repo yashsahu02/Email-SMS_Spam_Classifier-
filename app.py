52cc3c4e8cc4eac67b045daf8d5053111e6548e7
Yash Sahu <sahu.yash0205@gmail.com>
2024-12-31 14:26:24 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pickle 
 import string
 import nltk
-nltk.download('punkt', quiet=True)
+nltk.download('punkt')
 
 from nltk.corpus import stopwords
 from string import punctuation
 from nltk.stem.porter import PorterStemmer
 
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize 
 
 cv=pickle.load(open('models/count_vectorizer.pkl','rb'))
 scaler=pickle.load(open('models/scaler.pkl','rb'))
